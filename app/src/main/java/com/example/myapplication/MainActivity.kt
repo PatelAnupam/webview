343ac105708bcf6b3008672b11b04d61b8c4bcbb
PatelAnupam <anupam.patel@brightlifecare.com>
2020-04-10 12:30:08 +0530

    webview

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.myapplication
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.webkit.PermissionRequest
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+const val url = "https://5e90158f8747687dd236bd9c--suspicious-brahmagupta-b25f98.netlify.com"
+
+class MainActivity : AppCompatActivity() {
+    private val CAMERA_REQUEST_CODE = 101
+    private val TAG = "PermissionDemo"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val permission = ContextCompat.checkSelfPermission(this,
+            android.Manifest.permission.CAMERA)
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }else{
+            openWebView()
+        }
+    }
+
+    private fun openWebView(){
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.webViewClient = WebViewClient()
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.webChromeClient = object : WebChromeClient() {
+            override fun onPermissionRequest(request: PermissionRequest) {
+                request.grant(request.resources)
+            }
+        }
+        myWebView.loadUrl(url)
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE)
+    }
+
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "Permission has been denied by user")
+                } else {
+                    openWebView()
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if(webview.canGoBack()){
+            webview.goBack()
+        }else {
+            super.onBackPressed()
+        }
+    }
+
+}
+
